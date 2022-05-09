fba2e935769dda7b05521ce602ffcaf11c7e8f0b
Sophia Leeman <Sophia.Leeman@duke.edu>
2022-05-09 18:02:58 -0400

    Finish reading the file

--- Analyze.py
+++ Analyze.py
@@ -2,5 +2,11 @@
 import csv
 file = open('ckd_clean.csv')
 filereader = csv.reader(file)
+#Headings
 header = []
 header = next(filereader)
+#Records
+rows = []
+for row in filereader:
+    rows.append(row)
+file.close()
