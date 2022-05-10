6ea3cfca81f8b91ddecb4b714d664f5b8e6ae782
Sophia Leeman <Sophia.Leeman@duke.edu>
2022-05-10 11:33:11 -0400

    CKD patients exhibit significant albuminuria

--- Analyze.py
+++ Analyze.py
@@ -62,9 +62,9 @@ ckd = []
 #Assigning data
 for row in filereader:
     age.append(float(row[0]))
-    bp.append(row[1])
-    sg.append(row[2])
-    al.append(row[3])
+    bp.append(float(row[1]))
+    sg.append(float(row[2]))
+    al.append(float(row[3]))
     sug.append(row[4])
     rbcu.append(row[5])
     pus.append(row[6])
@@ -92,5 +92,31 @@ file.close()
 #Calculating Averages for Each Header Across the Whole Data Set and for CKD Patients Only
 #Average Age Across Data Set
 avage = sum(age)/len(age)
+print("The average age is:") 
+print(avage)
 #Average Age of CKD Patients
 ckdage = sum(age[0:43])/len(age[0:43]) #The first 43 subjects are ckd patients
+print("The average age for CKD patients is:")
+print(ckdage)
+#Blood Pressure
+avbp = sum(bp)/len(bp)
+print("The average bp is:")
+print(avbp)
+ckdbp = sum(bp[0:43])/len(bp[0:43])
+print("The average bp for CKD patients is:")
+print(ckdbp)
+#Specific Gravity
+avsg = sum(sg)/len(sg)
+print("The average sg is:")
+print(avsg)
+ckdsg = sum(sg[0:43])/len(sg[0:43])
+print("The average sg for CKD patients is:")
+print(ckdsg)
+#Albumin
+aval = sum(al)/len(al)
+print("The average al is:")
+print(aval)
+ckdal = sum(al[0:43])/len(al[0:43])
+print("The average al for CKD patients is:")
+print(ckdal) 
+#This shows the first striking difference, which is expected since Albuminuria is one of the main symptoms of CKD
